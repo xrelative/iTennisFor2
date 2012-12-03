7a25dcce103c1d4a6688f0ab0e92fe879f00f1a7
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2012-12-03 03:08:10 -0300

    reescrita toda la simulación física de la pelota

--- iTennisFor2/Classes/Bola.cpp
+++ iTennisFor2/Classes/Bola.cpp
@@ -31,13 +31,8 @@ coeficienteRestitucion(0.70f) // Sacado de canchas de verdad
 	//    CCAnimation *anim = CCAnimation::create();
 	anim->setDelayPerUnit(0.0);
 	anim->setLoops(INFINITY);
-	//    anim->addSpriteFrame(bola1);
-	//    anim->addSpriteFrame(bola2);
-	//    anim->addSpriteFrame(bola3);
-	//    anim->addSpriteFrame(bola4);
 	CCAnimate *theAnim = CCAnimate::create(anim);
 	sprite = CCSprite::createWithSpriteFrameName("ball1.png");
-	sprite->setPosition(ccp(size.width * 0.1, size.height * 0.75));
 	sprite->runAction(CCRepeatForever::create(theAnim));
 	batchNode->addChild(sprite, 0);
 	
@@ -45,111 +40,144 @@ coeficienteRestitucion(0.70f) // Sacado de canchas de verdad
 	
 	callbackMensaje = CCCallFuncO::create(pselectorCargaTarget, selectorCarga, &resultadoMensage);
 	callbackMensaje->retain(); // No estoy totalmente seguro por qué
+	
+	//todo de nuevo por la rctm:
+	t1  = 1.4;
+	
+	a   = -size.height*3;
+	
+	flr = piso;
+	
+	//inicio
+	xo = size.width  * 0.1;
+	yo = flr;
+	
+	trg = xo;
+	golpear(t1*0.5);
+	//todo de nuevo, fin
 }
 
-void Bola::update (float dt)
-{
-	velocidad.y += gravedad * dt;
-	CCPoint posicion = getPosicion();
+void Bola::golpear (float inicio) {
+	float dx = trg - xo;
+	dy = flr - yo;
+	t = inicio;
 	
-	CCPoint cambio = ccpMult(velocidad, dt);
-	posicion = ccpAdd(posicion, cambio);
+	vx = dx/t1;
+	vy = dy/t1 - t1*a*0.5;
+}
+
+void Bola::update (float dt) {
+	t+=dt;
 	
-	sprite->setPosition(posicion);
-	checkStatus();
+	float bx = xo+t*vx;
+	float by = yo+t*(vy+t*a*0.5);
+	
+	checkStatus(bx, by);
+	
+	if (t >= t1) {
+		float dx = trg - xo;
+		
+		xo = trg;
+		yo = flr;
+		
+		trg += dx + spin;
+		spin = 0;
+		
+		float exceso = t - t1;
+		golpear(exceso);
+		
+		//recalcular
+		bx = xo+t*vx;
+		by = yo+t*(vy+t*a*0.5);
+	}
+	
+	sprite->setPosition(ccp(bx, by));
 }
 
 void Bola::resetBall(int p){
+	t1  = 1.4;
+	
+	spin = 0;
 	funcionando = true;
-	if(p==1){
-		CCPoint posicion = getPosicion();
-		//posicion.x=size.width * 0.1;
-		//posi
-		velocidad.x=0;
-		velocidad.y=0;
-		sprite->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width*0.1, CCDirector::sharedDirector()->getWinSize().height * 0.50));
-		
-	}else{
-		velocidad.x=0;
-		velocidad.y=0;
-		sprite->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width*0.9, CCDirector::sharedDirector()->getWinSize().height * 0.50));
+	CCSize size = CCDirector::sharedDirector()->getWinSize();
+	CCPoint pos;
+	if (p==1) {
+		xo = size.width*0.1;
+	} else {
+		xo = size.width*0.9;
 	}
+	
+	yo = flr;
+	
+	trg = xo;
+	golpear(t1*0.5);
 }
 
 
-void Bola::checkStatus()
-{
-	CCPoint posicion = getPosicion();
-	
-	if(posicion.y < piso) {
-		colisionPiso();
-	}
+void Bola::checkStatus (float bx, float by) {
 	if (funcionando == false) {
 		return;
 	}
 	
-	if (posicion.x < 0){
+	if (bx < 0){
 		funcionando = false;
-		//		resetGame(); enviarEvento fuera de la pantalla por la izq
-		if(LastFloorColision==1){
+		//resetGame(); enviarEvento fuera de la pantalla por la izq
+		if (LastFloorColision==1) {
 			resultadoMensage.ScoreResult = 2;
 			callbackMensaje->execute();
-		}else{
+		} else {
 			resultadoMensage.ScoreResult = 1;
 			callbackMensaje->execute();
 		}
 	}
 	
 	CCSize size = CCDirector::sharedDirector()->getWinSize();
-	if (posicion.x > size.width){
+	if (bx > size.width){
 		funcionando = false;
-		//		resetGame(); enviarEvento fuera de la pantalla por la der
-		
-		
-		if(LastFloorColision==2){
+		//resetGame(); enviarEvento fuera de la pantalla por la der
+		if (LastFloorColision==2) {
 			resultadoMensage.ScoreResult = 1;
 			callbackMensaje->execute();
-		}else{
+		} else {
 			resultadoMensage.ScoreResult = 2;
 			callbackMensaje->execute();
 		}
-		
-		
 	}
 }
 
 #include <cmath>
 #include <algorithm>
 using namespace std;
-void Bola::colisionPiso()
-{
-	CCPoint posicion = getPosicion();
+void Bola::colisionPiso (float bx, float by) {
+//	CCSize size = CCDirector::sharedDirector()->getWinSize();
+//	LastFloorColision = (bx > size.width/2) ? 2 : 1;
+//	
+//	float dx = trg - xo;
+//	
+//	xo = trg;
+//	yo = flr;
+//	
+//	trg += dx;
+//	
+//	float exceso = t - t1;
+//	golpear();
+//
+//	float direccionX = velocidad.x>=0 ? 1.0 : -1.0;
+//	//	velocidad = ccpRotateByAngle(velocidad, CCPointZero, spin);
+//	float fuerzaSpin = spin*200.0; // momento rotacional -> lineal
+//	float velocidadMinima =  200.0;
+//	float velocidadMaxima = 1000.0;
+//	
+//	if (velocidad.x != 0) { // Así se mantiene rebotando antes del primer golpe.
+//		velocidad.x = direccionX * max(velocidadMinima, min((fabs(velocidad.x+fuerzaSpin)), velocidadMaxima));
+//		velocidad.y *= -coeficienteRestitucion;
+//	} else {
+//		CCSize size = CCDirector::sharedDirector()->getWinSize();
+//		velocidad.y = size.height;
+//	}
+//	spin *= 0.1; // perdida de energia por roce
+//
 	
-	posicion.y = piso;
-	
-	if(posicion.x>CCDirector::sharedDirector()->getWinSize().width/2){
-		LastFloorColision=2;
-	}else{
-		LastFloorColision=1;
-	}
-	
-	
-	float direccionX = velocidad.x>=0 ? 1.0 : -1.0;
-	//	velocidad = ccpRotateByAngle(velocidad, CCPointZero, spin);
-	float fuerzaSpin = spin*200.0; // momento rotacional -> lineal
-	float velocidadMinima =  200.0;
-	float velocidadMaxima = 1000.0;
-	
-	if (velocidad.x != 0) { // Así se mantiene rebotando antes del primer golpe.
-		velocidad.x = direccionX * max(velocidadMinima, min((fabs(velocidad.x+fuerzaSpin)), velocidadMaxima));
-		velocidad.y *= -coeficienteRestitucion;
-	} else {
-		CCSize size = CCDirector::sharedDirector()->getWinSize();
-		velocidad.y = size.height;
-	}
-	spin *= 0.1; // perdida de energia por roce
-	
-	sprite->setPosition(posicion);
 }
 
 CCPoint Bola::getPosicion () {
